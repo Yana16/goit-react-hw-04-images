b44ccea6ee78426b097003b2d87025c06a18f573
Yana Vozniuk <yana.vozniuk@ukr.net>
2023-02-18 11:25:12 +0200

    hw
    hw

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,45 +1,32 @@
 import style from '../Modal/modal.module.css';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
-import PropTypes from 'prop-types';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.closeModal);
-  }
+const Modal = ({ onClose, children }) => {
+  useEffect(() => {
+    const closeModal = ({ target, currentTarget, code }) => {
+      if (target === currentTarget || code === 'Escape') {
+        onClose();
+      }
+    };
+    document.addEventListener('keydown', closeModal);
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.closeModal);
-  }
+    return () => document.removeEventListener('keydown', closeModal);
+  }, [onClose]);
 
-  closeModal = ({ target, currentTarget, code }) => {
-    if (target === currentTarget || code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    const { children, onClose } = this.props;
-    const { closeModal } = this;
-    return createPortal(
-      <div className={style.Overlay} onClick={closeModal}>
-        <div className={style.Modal}>
-          {children}
-          <span className={style.Close} onClick={onClose}>
-            X
-          </span>
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <div className={style.Overlay} onClick={onClose}>
+      <div className={style.Modal}>
+        {children}
+        <span className={style.Close} onClick={onClose}>
+          X
+        </span>
+      </div>
+    </div>,
+    modalRoot
+  );
+};
 
 export default Modal;
-
-Modal.propTypes = {
-  children: PropTypes.object.isRequired,
-  onClose: PropTypes.func.isRequired,
-};
